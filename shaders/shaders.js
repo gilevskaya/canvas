5e143cfb5ca20f568b2022740abba96d6ca3acb2
Alena Gilevskaya <alena@gilevskaya.com>
2020-11-02 22:40:26 -0800

    basic shader

---
+++ shaders/shaders.js
@@ -0,0 +1,66 @@
+(() => {
+  window.addEventListener("load", setupWebGL, false);
+
+  let gl;
+  let program;
+
+  function setupWebGL(evt) {
+    window.removeEventListener(evt.type, setupWebGL, false);
+    if (!(gl = getRenderingContext())) return;
+
+    var source = document.querySelector("#vertex-shader").innerHTML;
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, source);
+    gl.compileShader(vertexShader);
+    source = document.querySelector("#fragment-shader").innerHTML;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, source);
+    gl.compileShader(fragmentShader);
+    program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      var linkErrLog = gl.getProgramInfoLog(program);
+      cleanup();
+      console.error(linkErrLog);
+      return;
+    }
+
+    initializeAttributes();
+
+    gl.useProgram(program);
+    gl.drawArrays(gl.POINTS, 0, 1);
+
+    cleanup();
+  }
+
+  var buffer;
+  function initializeAttributes() {
+    gl.enableVertexAttribArray(0);
+    buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(0, 1, gl.FLOAT, false, 0, 0);
+  }
+
+  function cleanup() {
+    gl.useProgram(null);
+    if (buffer) gl.deleteBuffer(buffer);
+    if (program) gl.deleteProgram(program);
+  }
+
+  function getRenderingContext() {
+    const canvas = document.getElementById("webgl-canvas");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    const gl = canvas.getContext("webgl");
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    return gl;
+  }
+})();
