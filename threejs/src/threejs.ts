7a4c32d338bcb720941cc03d6f522db247c26b38
Alena Gilevskaya <alena@gilevskaya.com>
2020-11-07 15:35:59 -0800

    2 candles

--- threejs/src/threejs.ts
+++ threejs/src/threejs.ts
@@ -1,47 +1,78 @@
 /// <reference path="../node_modules/three/src/three.d.ts" />
 
-console.log("threejs", THREE);
+const OHLC = [
+  {
+    price: 9900,
+    open: 10,
+    high: 15,
+    low: 3,
+    close: 5,
+  },
+];
 
 const canvas = document.getElementById("webgl-canvas") as HTMLCanvasElement;
+const { height, width } = canvas;
+console.log({ height, width });
 
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setClearColor("hsl(0, 0%, 95%)", 1.0);
+renderer.setClearColor(0x010101, 1.0);
 
 // Setup a camera, we will update its settings on resize
-const camera = new THREE.PerspectiveCamera(30, 1, 0.01, 100);
-camera.position.set(2, 2, 2);
-camera.lookAt(new THREE.Vector3());
+const camera = new THREE.OrthographicCamera(
+  width / -width,
+  width / width,
+  height / height,
+  height / -height,
+  1,
+  1000
+);
+camera.position.z = 1;
+// camera.position.set(2, 2, 2);
+// camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-// const controls = new THREE.OrbitControls(camera);
 const scene = new THREE.Scene();
 
-// Re-use the same Geometry across all our cubes
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const {
+  candleBodyBuy,
+  candleBodySell,
+  candleWickBuy,
+  candleWickSell,
+} = getCandleParts();
 
-// Basic "unlit" material with no depth
-const material = new THREE.MeshNormalMaterial();
-const material2 = new THREE.MeshStandardMaterial({
-  color: "white",
-  roughness: 0.75,
-  flatShading: true,
-});
+scene.add(candleBodyBuy.translateX(-0.5));
+scene.add(candleWickBuy.translateX(-0.5));
+scene.add(candleBodySell.translateX(0.5));
+scene.add(candleWickSell.translateX(0.5));
 
-// Create the mesh
-const mesh = new THREE.Mesh(geometry, material2);
+// var path = new THREE.Shape();
+// path.currentPoint = new THREE.Vector2(-1, 1);
+// path.lineTo(1, 1);
+// path.lineTo(1, -1);
+// path.lineTo(-1, -1);
+// path.autoClose = true;
 
-// Smaller cube
-mesh.scale.setScalar(0.5);
-
-// Then add the group to the scene
-scene.add(mesh);
-
-scene.add(new THREE.AmbientLight("#59314f"));
-const light = new THREE.PointLight("#45caf7", 1, 15.5);
-light.position.set(2, 2, -4).multiplyScalar(1.5);
-scene.add(light);
-
-const mesh2 = new THREE.Mesh(geometry, material2);
-mesh2.scale.setScalar(0.5);
-scene.add(mesh2);
+// var candleGeometry = new THREE.ShapeBufferGeometry(path);
+// var ellipse = new THREE.Mesh(candleGeometry, materialGreen);
+// scene.add(ellipse);
 
 renderer.render(scene, camera);
+
+function getCandleParts() {
+  const materialBuy = new THREE.MeshBasicMaterial({ color: 0x22833d });
+  const materialSell = new THREE.MeshBasicMaterial({ color: 0xb82e40 });
+
+  const candleBodyGeometry = new THREE.PlaneGeometry(1, 1, 1); // rect proportional to canvas size
+
+  const wick = new THREE.Path();
+  wick.currentPoint = new THREE.Vector2(0, 1);
+  wick.lineTo(0, -1);
+  const points = wick.getPoints();
+  const wickLine = new THREE.BufferGeometry().setFromPoints(points);
+
+  const candleBodyBuy = new THREE.Mesh(candleBodyGeometry, materialBuy);
+  const candleBodySell = new THREE.Mesh(candleBodyGeometry, materialSell);
+  const candleWickBuy = new THREE.Line(wickLine, materialBuy);
+  const candleWickSell = new THREE.Line(wickLine, materialSell);
+
+  return { candleBodyBuy, candleBodySell, candleWickBuy, candleWickSell };
+}
