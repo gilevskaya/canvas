02a757af25c535ef0c340249dab0bec851d2d559
Alena Gilevskaya <alena@gilevskaya.com>
2020-11-07 19:20:13 -0800

    un-resized candles

--- threejs/src/threejs.ts
+++ threejs/src/threejs.ts
@@ -23,34 +23,35 @@ const OHLC: TCandle[] = [
     low: 3,
     close: 5,
   },
-  // {
-  //   timestamp: 10,
-  //   open: 5,
-  //   high: 17,
-  //   low: 4,
-  //   close: 10,
-  // },
-  // {
-  //   timestamp: 15,
-  //   open: 10,
-  //   high: 15,
-  //   low: 3,
-  //   close: 5,
-  // },
-  // {
-  //   timestamp: 20,
-  //   open: 5,
-  //   high: 17,
-  //   low: 4,
-  //   close: 10,
-  // },
+  {
+    timestamp: 10,
+    open: 5,
+    high: 17,
+    low: 4,
+    close: 10,
+  },
+  {
+    timestamp: 15,
+    open: 10,
+    high: 15,
+    low: 3,
+    close: 5,
+  },
+  {
+    timestamp: 20,
+    open: 5,
+    high: 17,
+    low: 4,
+    close: 10,
+  },
 ];
 const priceRange: [number, number] = [0, 20];
+const priceStep = 5;
 const timeRange: [number, number] = [5, 20];
+const timeStep = 5;
 
 const canvas = document.getElementById("webgl-canvas") as HTMLCanvasElement;
 const { height, width } = canvas;
-console.log({ height, width });
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setClearColor(0x141414, 1.0);
@@ -65,9 +66,9 @@ const scene = new THREE.Scene();
 const candleParts = getCandleParts();
 // cubeA.position.set( 100, 100, 0 );
 
-const scale: [number, number, number] = [0.9, 0.9, 1];
+const scale: [number, number, number] = [0.2, 0.8, 1];
 
-const grid = makeGrid(timeRange, 5, priceRange, 5);
+const grid = makeGrid(timeRange, timeStep, priceRange, priceStep);
 grid.scale.set(...scale);
 grid.position.setZ(-1);
 scene.add(grid);
@@ -75,11 +76,11 @@ scene.add(grid);
 const candlesGroup = new THREE.Group();
 const candles: THREE.Group[] = [];
 OHLC.forEach((candleData) => {
-  candles.push(makeCandle(candleParts, candleData, timeRange, priceRange));
+  candles.push(makeCandle(candleParts, candleData, timeRange, timeStep, priceRange));
 });
 candles.forEach((c) => candlesGroup.add(c));
 candlesGroup.scale.set(...scale);
-// scene.add(candlesGroup);
+scene.add(candlesGroup);
 
 renderer.render(scene, camera);
 
@@ -90,18 +91,32 @@ function getCandleParts() {
   const materialLineBuy = new THREE.LineBasicMaterial({ color: 0x22833d, linewidth: 2 });
   const materialLineSell = new THREE.LineBasicMaterial({ color: 0xb82e40, linewidth: 2 });
 
-  const candleBodyGeometry = new THREE.PlaneGeometry(0.9, 1, 1); // rect proportional to canvas size
+  // const candleBodyGeometry = new THREE.PlaneGeometry(0.49, 1, 1); // rect proportional to canvas size
 
-  const wick = new THREE.Path();
-  wick.currentPoint = new THREE.Vector2(0, 1);
-  wick.lineTo(0, -1);
-  const points = wick.getPoints();
-  const wickLine = new THREE.BufferGeometry().setFromPoints(points);
+  const makeCandleBody = (() => {
+    const shape = new THREE.Shape();
+    shape.currentPoint = new THREE.Vector2(0, 0);
+    shape.setFromPoints([
+      new THREE.Vector2(c(0), c(0)),
+      new THREE.Vector2(c(0), c(1)),
+      new THREE.Vector2(c(1), c(1)),
+      new THREE.Vector2(c(1), c(0)),
+    ]);
+    const geometry = new THREE.ShapeBufferGeometry(shape);
+    return (material) => new THREE.Mesh(geometry, material);
+  })();
 
-  const candleBodyBuy = new THREE.Mesh(candleBodyGeometry, materialBuy);
-  const candleBodySell = new THREE.Mesh(candleBodyGeometry, materialSell);
-  const candleWickBuy = new THREE.Line(wickLine, materialLineBuy);
-  const candleWickSell = new THREE.Line(wickLine, materialLineSell);
+  const makeCandleWick = (() => {
+    const path = new THREE.Path();
+    path.setFromPoints([new THREE.Vector2(0, -1), new THREE.Vector2(0, 1)]);
+    const geometry = new THREE.BufferGeometry().setFromPoints(path.getPoints());
+    return (material) => new THREE.Line(geometry, material);
+  })();
+
+  const candleBodyBuy = makeCandleBody(materialBuy);
+  const candleBodySell = makeCandleBody(materialSell);
+  const candleWickBuy = makeCandleWick(materialLineBuy);
+  const candleWickSell = makeCandleWick(materialLineSell);
 
   return { candleBodyBuy, candleBodySell, candleWickBuy, candleWickSell };
 }
@@ -110,23 +125,30 @@ function makeCandle(
   candleParts: TCandleParts,
   candleData: TCandle,
   timeRange: [number, number],
+  timeStep: number,
   priceRange: [number, number]
 ) {
   const { candleBodyBuy, candleBodySell, candleWickBuy, candleWickSell } = candleParts;
   const { timestamp, open, high, low, close } = candleData;
   const [timeMin, timeMax] = timeRange;
+  const x = (timestamp - timeStep) / (timeMax - timeMin);
   const [priceMin, priceMax] = priceRange;
 
-  const candleBody = open > close ? candleBodySell : candleBodyBuy;
-  const candleWick = open > close ? candleWickSell : candleWickBuy;
-  const x = timestamp / (timeMax - timeMin);
+  const candleBody = (open > close ? candleBodySell : candleBodyBuy).clone();
+  const candleWick = (open > close ? candleWickSell : candleWickBuy).clone();
+  console.log("x", x);
 
   const wickH = Math.abs(low - high) / priceMax - priceMin;
-  candleWick.scale.set(1, wickH * 2, 1);
+  candleBody.scale.set(1, 0.5, 1);
+  // candleWick.scale.set(1, wickH * 2, 1);
+  candleWick.translateX(c(x));
 
   const candleGroup = new THREE.Group();
-  candleGroup.add(candleBody.clone().translateX(x));
-  candlesGroup.add(candleWick.clone().translateX(x));
+  console.log("c(x)", c(x));
+  candleGroup.add(candleBody);
+  candlesGroup.add(candleWick);
+  candleGroup.scale.set(0.25, 1, 1);
+  candleGroup.position.set(c(x), 0, 0);
   return candleGroup;
 }
 
@@ -135,22 +157,21 @@ function makeGrid(timeRange, timeStep, priceRange, priceStep) {
   const stepX = timeStep / (timeMax - timeMin);
   const [priceMin, priceMax] = priceRange;
   const stepY = priceStep / (priceMax - priceMin);
-  console.log({ stepX, stepY });
 
   const gridLineMaterial = new THREE.LineBasicMaterial({
     color: 0x393939,
     linewidth: 0.5,
   }); // 0x1d1d1d | 010101
 
-  function getBaseLine(x1: number, y1: number, x2: number, y2: number) {
+  function makeBaseLine(x1: number, y1: number, x2: number, y2: number) {
     const path = new THREE.Path();
     path.setFromPoints([new THREE.Vector2(x1, y1), new THREE.Vector2(x2, y2)]);
     const geometry = new THREE.BufferGeometry().setFromPoints(path.getPoints());
     return new THREE.Line(geometry, gridLineMaterial);
   }
   const gridGroup = new THREE.Group();
-  const gridLineY = getBaseLine(c(0), c(0), c(0), c(1));
-  const gridLineX = getBaseLine(c(0), c(0), c(1), c(0));
+  const gridLineY = makeBaseLine(c(0), c(0), c(0), c(1));
+  const gridLineX = makeBaseLine(c(0), c(0), c(1), c(0));
 
   for (let currXT = 0; currXT <= 1; currXT += stepX) {
     gridGroup.add(gridLineY.clone().translateX(currXT * 2));
